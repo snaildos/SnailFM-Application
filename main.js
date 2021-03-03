b14760030731e01d2a79fdff4cebe2e6f8e89bfa
SnailDOS <46210117+snaildos@users.noreply.github.com>
2021-03-03 12:21:39 +0800

    Let's try autoupdate again.
    I think this should work.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+const { autoUpdater } = require('electron-updater');
 const { SSL_OP_EPHEMERAL_RSA } = require('constants');
 const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
 const { watchFile } = require('fs');
 
 
@@ -95,5 +95,21 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
 });
 
