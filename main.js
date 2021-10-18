9282de127f04dff2a751b1363bf558cf7730977b
Restyled.io <commits@restyled.io>
2021-10-18 00:50:49 +0000

    Restyled by clang-format

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-const { autoUpdater } = require('electron-updater');
-const { SSL_OP_EPHEMERAL_RSA } = require('constants');
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { watchFile } = require('fs');
-const { trackEvent } = require('./lib/analytics.js');
+const {autoUpdater} = require('electron-updater');
+const {SSL_OP_EPHEMERAL_RSA} = require('constants');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const {watchFile} = require('fs');
+const {trackEvent} = require('./lib/analytics.js');
 const glasstron = require('glasstron');
 const electron = require('electron');
 // Notify
-const { Notification } = require('electron')
+const {Notification} = require('electron')
 
 function neterr() {
   const notification = {
-    title: 'SnailFM',
-    body: 'No valid network connection! Please reconnect!'
+    title : 'SnailFM',
+    body : 'No valid network connection! Please reconnect!'
   }
 }
 
 // wait function
-function wait(ms)
-{
-    var d = new Date();
-    var d2 = null;
-    do { d2 = new Date(); }
-    while(d2-d < ms);
+function wait(ms) {
+  var d = new Date();
+  var d2 = null;
+  do {
+    d2 = new Date();
+  } while (d2 - d < ms);
 }
 
 // Start the libaries
@@ -29,43 +29,42 @@ require('./lib/rpc.js');
 console.log("RPC lib init.");
 
 // Blur Service
-if (process.platform == 'darwin') { 
+if (process.platform == 'darwin') {
   app.whenReady().then(() => { // macOS
     global.blurType = "vibrancy";
     global.windowFrame = 'false'
-})}
-else if(process.platform == 'win32'){ 
+  })
+} else if (process.platform == 'win32') {
   app.whenReady().then(() => { // Windows
     global.blurType = "acrylic";
-    global.windowFrame = 'false' // The effect won't work properly if the frame is enabled on Windows
-})}
-else{ 
+    global.windowFrame = 'false' // The effect won't work properly if the frame
+                                 // is enabled on Windows
+  })
+} else {
   app.whenReady().then(() => { // Linux
     global.blurType = "blurbehind";
     global.windowFrame = 'true'
-})}
+  })
+}
 
 // Loading screen
 /// create a global var, wich will keep a reference to out loadingScreen window
 let loadingScreen;
 const createLoadingScreen = () => {
-  loadingScreen = new BrowserWindow(
-    Object.assign({
-      width: 700,
-      height: 120,
-      alwaysOnTop: true,
-      frame: false,
-      fullscreen: false,
-      show: true,
-      transparent: true
-    })
-  );
+  loadingScreen = new BrowserWindow(Object.assign({
+    width : 700,
+    height : 120,
+    alwaysOnTop : true,
+    frame : false,
+    fullscreen : false,
+    show : true,
+    transparent : true
+  }));
   loadingScreen.setResizable(false);
   loadingScreen.loadFile('splash.html');
   loadingScreen.on('closed', () => (loadingScreen = null));
-  loadingScreen.webContents.on('did-finish-load', () => {
-    loadingScreen.show();
-  });
+  loadingScreen.webContents.on('did-finish-load',
+                               () => { loadingScreen.show(); });
 };
 console.log("Loading screen ready.");
 
@@ -74,21 +73,21 @@ electron.app.commandLine.appendSwitch("enable-transparent-visuals");
 // Start the main program
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new glasstron.BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false,
-    titlebarStyle: 'hiddenInset',
-    fullscreen: false,
-    blur: true,
-    blurType: global.blurType,
-    modal: true,
-    icon: 'snailfm.ico',
-    webPreferences: {
-      nodeIntegration: true,
-      nativeWindowOpen: true,
-      contextIsolation: false
+    width : 800,
+    height : 600,
+    show : false,
+    titlebarStyle : 'hiddenInset',
+    fullscreen : false,
+    blur : true,
+    blurType : global.blurType,
+    modal : true,
+    icon : 'snailfm.ico',
+    webPreferences : {
+      nodeIntegration : true,
+      nativeWindowOpen : true,
+      contextIsolation : false
     },
   });
   mainWindow.setMenuBarVisibility(false)
@@ -102,37 +101,32 @@ function createWindow () {
     }
     var isDev = require('isdev')
 
-if(isDev) {
-  console.log("In Development!")
-} else {
-  console.log("Not in Development!")
-}
+    if (isDev) {
+      console.log("In Development!")
+    }
+    else {console.log("Not in Development!")}
 
     var internetAvailable = require("internet-available");
 
-// Set a timeout and a limit of attempts to check for connection
-internetAvailable({
-    timeout: 2000,
-    retries: 3,
-}).then(function(){
-    console.log("Internet available");
-}).catch(function(){
-    console.log("No internet");
-    neterr()
-    mainWindow.loadFile('nonet.html');
-    new Notification(notification).show()
-});
+    // Set a timeout and a limit of attempts to check for connection
+    internetAvailable({
+      timeout : 2000,
+      retries : 3,
+    })
+        .then(function() { console.log("Internet available"); })
+        .catch(function() {
+          console.log("No internet");
+          neterr()
+          mainWindow.loadFile('nonet.html');
+          new Notification(notification).show()
+        });
     mainWindow.show();
     console.log("Ok! Window init, let's check for updates...")
     autoUpdater.checkForUpdatesAndNotify();
     console.log("Update checked. Let's see what happens!");
   });
 
-  
-  
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+  mainWindow.on('closed', function() { mainWindow = null; });
 }
 
 console.log("Main screen ready.");
@@ -141,34 +135,25 @@ app.on('ready', () => {
   createLoadingScreen();
   console.log("Send check for updates signal...");
   console.log("Alright, lets go!");
-  setTimeout(() => {
-    createWindow();
-  }, 3000);
+  setTimeout(() => { createWindow(); }, 3000);
 })
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+  event.sender.send('app_version', {version : app.getVersion()});
 });
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
-});
+autoUpdater.on('update-available',
+               () => { mainWindow.webContents.send('update_available'); });
 
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
-});
+autoUpdater.on('update-downloaded',
+               () => { mainWindow.webContents.send('update_downloaded'); });
 
-ipcMain.on('relaunch', () => {
-  mainWindow.close();
-});
+ipcMain.on('relaunch', () => { mainWindow.close(); });
 
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
-
+ipcMain.on('restart_app', () => { autoUpdater.quitAndInstall(); });
