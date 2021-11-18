22bd6d3545c7d8647c0b6724732978649a4e1d60
SnailDOS <zarasoonich@hotmail.com>
2021-11-18 12:46:49 +0800

    The Login System IMPLEMENTED!
    This has taken over ~1 day straight (24 hours) of work to implement this bug free working login system. The login system features token validation and more.
    Error handling such as 413 is to come as well as useable actual features in the next update. Pretty much the same code will be used in StreamBop and SnailPortal.
    FEATURES THAT NEED TO BE ADDED:
    - Error HANDLING. :sweat:
    - STABILITY.
    - More CHECKS.
    - Better language responses
    SECURITY. and such as performance.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const { autoUpdater } = require("electron-updater");
 const { SSL_OP_EPHEMERAL_RSA } = require("constants");
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, protocol, ipcRenderer } = require("electron");
 const { watchFile } = require("fs");
 const { trackEvent } = require("./lib/analytics.js");
 const glasstron = require("glasstron");
 const electron = require("electron");
 const isOnline = require('is-online');
+const Store = require("electron-store");
+const store = new Store();
+const axios = require('axios');
+token = store.get('token')
 // Notify
 const { Notification } = require("electron");
 electron.app.commandLine.appendSwitch("enable-transparent-visuals");
@@ -101,6 +105,9 @@ function createWindow() {
   mainWindow.setResizable(false);
   mainWindow.loadFile("index.html");
   mainWindow.on("maximize", () => mainWindow.unmaximize());
+  ipcMain.on("main_show", () => mainWindow.show());
+  ipcMain.on('main_hide', () => {mainWindow.hide();})
+  ipcMain.on("load_acc", () => mainWindow.loadFile('myaccount.html'))
   mainWindow.webContents.on("did-finish-load", () => {
     if (loadingScreen) {
       loadingScreen.close();
@@ -139,13 +146,133 @@ function createWindow() {
 }
 
 console.log("Main screen ready.");
+console.log("Setup login function...");
+var loginclose = 0
+function createloginWindow() {
+  const loginWindow = (new BrowserWindow({ 
+    width: 1200,
+    height: 700,
+    minWidth: 800,
+    minHeight: 400,
+    center: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+  }
+  }));
+  loginWindow.on('close', function (event) {
+    event.preventDefault();
+    const notification4 = {
+      title: "SnailFM",
+      body: "This window won't close unless you login! If you want to exit SnailFM, press the close button 3 times!",
+    };        
+    loginclose = loginclose + 1;
+    if (loginclose === 3) {
+      store.set('logged_in', false)
+      console.log("App quitting...")
+      ipcMain.emit('appquit')
+    } else {
+      if (loginclose === 1) {
+        new Notification(notification4).show();
+      }
+    }
+  
+  });
+
+  loginWindow.hide()
+  loginWindow.setMenuBarVisibility(false) 
+  ipcMain.on('login_sar', () => {loginWindow.show(); loginWindow.loadURL("https://login.snaildos.com?redir=snailfm://login"); loginWindow.maximize(); ipcMain.emit('main_hide')})
+  ipcMain.on('login_show', () => {loginWindow.show(); loginWindow.maximize(); loginWindow.restore()})
+  ipcMain.on('login_hide', () => {loginWindow.hide();})
+  ipcMain.on('login_exit', () => {loginWindow.destroy();})
+  loginWindow.loadURL("https://login.snaildos.com?redir=snailfm://login");
+}
+app.whenReady().then(() => {
+  ipcMain.on('token_verify', function() {
+    if ((store.get('logged_in') === false)) {
+      console.log("All good! Login is disabled...");
+      createWindow()
+    } else {
+  if (token != null) {
+    console.log("Verifying Identity..")
+    async function verify() {
+      await axios.get('https://api.snaildos.com/users/@me', {
+      headers: {
+      Authorization: token
+      }}
+      )
+        .then(function (response) {
+          // handle success
+          if (( response.data.status ) === "false") {
+            config.set('token', null)
+            console.log("Token failure!")
+            ipcMain.emit("login_sar")
+            loadingScreen.close();
+          } else {
+            console.log("Sucess!")
+            createWindow();
+          }
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+          ipcMain.emit("login_sar")
+          console.log("Token failure!")
+          loadingScreen.close();
+        })
+        .then(function () {
+        });
+      }
+      verify()
+  } else {
+    if ((store.get('logged_in') === false)) {
+      console.log("All good! Login is disabled...");
+      createWindow()
+    } else {
+      if ((store.get('setup') != undefined)) {
+      console.log("User has login enabled but is not logged in, showing login screen...");
+      ipcMain.emit("login_sar")
+      console.log("Token failure!")
+      loadingScreen.close();
+      } else {
+        createWindow()
+      }
+    }
+  }
+}
+})
+  
+  app.setAsDefaultProtocolClient("snailfm");
+  protocol.registerHttpProtocol('snailfm', (req, cb) => {
+    const url = req.url.substr(22)
+      console.log("Logging in: "+url);
+      console.log("Token data: "+url)
+      store.set('token', url);
+      ipcMain.emit('load_acc');
+      ipcMain.emit('login_hide')
+      ipcMain.emit('main_show')
+      store.set('logged_in', true);
+  })
+  app.on("open-url", (event, url) => {
+    const url2 = req.url.substr(22)
+    console.log("Logging in: "+url2);
+    console.log("Token data: "+url2)
+    store.set('token', url2);
+    ipcMain.emit('load_acc');
+    ipcMain.emit('login_hide')
+    ipcMain.emit('main_show')
+    store.set('logged_in', true);
+  });
+});
+
 
 app.on("ready", () => {
   createLoadingScreen();
   console.log("Send check for updates signal...");
   console.log("Alright, lets go!");
   setTimeout(() => {
-    createWindow();
+    createloginWindow()
+    ipcMain.emit('token_verify');
   }, 3000);
 });
 
@@ -175,7 +302,8 @@ ipcMain.on("restart_app", () => {
   autoUpdater.quitAndInstall();
 });
 
+ipcMain.on('appquit', () => {app.exit()})
 ipcMain.on('minimize', () => {mainWindow.minimize()})
 ipcMain.on('maximize', () => {mainWindow.maximize()})
 ipcMain.on('restore', () => {mainWindow.restore()})
-ipcMain.on('close', () => {mainWindow.close()})
+ipcMain.on('close', () => {mainWindow.close(); ipcMain.emit('login_exit')})
